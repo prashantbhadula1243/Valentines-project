529bc7d0c410b04485cacfcb64258db7f47fbfde
Prashant bhadula <yashbhandari617@gmail.com>
2026-02-13 23:23:25 +0530

    Update messages array with new prompts

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
+    "pakka?",
+    "dekhle abhi bhi time hai??",
+    "Ab nahi dabna chahiye no?",
+    "acha sorry please...",
+    "sochle momos khilaunga!",
+    "agar na bola toh fir rounga",
     "I will be very sad...",
     "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "last time hai han boldo mere jaisa nahi milega",
+    "Just say yes my pooku love you ❤️"
 ];
 
 let messageIndex = 0;
